28366780789b62f308c0827c918b371afd8783ba
simalve <sispeed@vassar.edu>
2016-10-09 22:03:00 -0400

    aic&bic formulas and parameter fitting for optic using 2 parameters, then 1

--- model-fitting.R
+++ model-fitting.R
@@ -8,23 +8,25 @@ rm(sample.training.data)
 # Note: In optim() you can tell it to display updates as it goes with:
 # optim( ... , control=list(trace=4))
 
-linear.model.error <- function(parameters){
-  b <- parameters[1]
-  m <- parameters[2]
-  
-  predicted.y <- b + m*x
-  
-  sq.error <- (y - predicted.y)^2
-  rmse <- sqrt(mean(sq.error))
-  
-  return(rmse)
-}
+#linear.model.error <- function(parameters){
+#  b <- parameters[1]
+#  m <- parameters[2]
+#  
+#  predicted.y <- b + m*x
+#  
+#  sq.error <- (y - predicted.y)^2
+#  rmse <- sqrt(mean(sq.error))
+#  
+# return(rmse)
+#}
 
-optim4data <- function(parameters){
+optim4data <- function(parameters){ 
   sensitivity <- parameters[1]
   decay.rate <- parameters[2]
-  
+      exemplar.memory.log.likelihood (all.data, sensitivity, decay.rate)
 }
+
+optim(c(1,1), optim4data, method="Nelder-Mead")
 
 # Now try fitting a restricted version of the model, where we assume there is no decay.
 # Fix the decay.rate parameter to 1, and use optim to fit the sensitivity parameter.
@@ -32,10 +34,64 @@ optim4data <- function(parameters){
 # The brent method also requires an upper and lower boundary:
 # optim( ..., upper=100, lower=0, method="Brent")
 
+optim4data.fixed <- function(parameters){ 
+  sensitivity <- parameters[1]
+  exemplar.memory.log.likelihood (all.data, sensitivity, 1)
+}
+
+# optim4data.fixed (0.33)
+
+optim(c(1), optim4data.fixed, upper=100, lower=0, method="Brent") #note: vector c only has one value instead of 2 because there is only one parameter this time
+
 # What's the log likelihood of both models? (see the $value in the result of optiom(),
 # remember this is the negative log likeihood, so multiply by -1.
+#ANSWER:
+#Log Likelihood of non-fixed model is 345.1454
+#Log likelihood of fixed model is 1241.907
+
 
 # What's the AIC and BIC for both models? Which model should we prefer?
+#help(AIC)
+#AIC(optim4data)
+#AIC(optim4data, k=2)
+
+#Akaike Information Criterion (AIC) = 2k - 2ln(L)
+#Where k = the number of free parameters
+#L = the maximum likelihood
+#N = the sample size
+k.aic.nonfixed <- 2
+L.aic.nonfixed <- 345.1454
+N.aic.nonfixed <- 500
+answer.aic.nonfixed <- 2*2 - 2(log(345.1454))
+
+answer.aic.nonfixed
+
+k.aic.fixed <- 1
+L.aic.fixed < 1241.907
+N.aic.fixed <- 500
+answer.aic.fixed <- 2*1 - 2(log(1241.907))
+
+answer.aic.fixed
+
+exp((AICbest - AICcomparison) / 2)
+
+#Bayesian Information Criterion (BIC) = k*ln(N) - 2ln(L)
+#Where k = the number of free parameters
+#L = the maximum likelihood
+#N = the sample size
+k.bic.nonfixed <- 2
+L.bic.nonfixed <- 345.1454
+N.bic.nonfixed <- 500
+answer.bic.nonfixed <- 2*log(500) - 2(log(345.1454))
+
+answer.bic.nonfixed
+
+k.bic.fixed <- 1
+L.bic.fixed < 1241.907
+N.bic.fixed <- 500
+answer.bic.fixed <- 1*log(500) - 2(log(1241.907))
+
+
 
 #### BONUS...
 # If you complete this part I'll refund you a late day. You do not need to do this.
